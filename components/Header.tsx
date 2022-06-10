24c2977b2aa040803e8a286d5f3643c50dca2ede
Houdaifa <chergaoui@houdaifa.com>
2022-06-10 12:15:37 +0100

    Fix small logo bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Header = () => {
   return (
     <header>
-      <Image src="/images/logo.png" alt="logo" width={117} height={39} />
+      <Image src="/images/Logo.png" alt="logo" width={117} height={39} />
       <a className="link-contact">GET IN TOUCH</a>
     </header>
   );
